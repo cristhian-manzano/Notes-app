1cb6587a58ca1de85c875498122d1c6e46c9367b
Cristhian Manzano <crisven1998@gmail.com>
2022-02-24 18:25:17 -0500

    Adding husky pre-commit - prettier and eslint validations

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,6 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res, next) => {
-  const x = 'hello';
-
   try {
     res.status(201).json({ message: 'Good!' });
   } catch (e) {
